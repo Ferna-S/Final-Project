b59cf7985fb8fd9e887e52542c67cb9f89f6134b
FernaS <>
2021-04-08 03:17:53 +0000

    a couple edits

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-const one = document.querySelector('.red');
-const two = document.querySelector('.green');
-const three = document.querySelector('.blue');
-const four = document.querySelector('.yellow');
-const five = document.querySelector('.orange');
-const six = document.querySelector('.purple');
-const seven = document.querySelector('.cyan');
-const eight = document.querySelector('.indigo');
-const nine = document.querySelector('.coral');
-const ten = document.querySelector('.maroon');
-const eleven = document.querySelector('.seagreen');
-const twelve = document.querySelector('.grey');
-const thirteen = document.querySelector('.salmon');
-const fourteen = document.querySelector('.olive');
-const fifteen = document.querySelector('.navy');
-const sixteen = document.querySelector('.saddlebrown');
+const one = document.querySelector('.red')
+const two = document.querySelector('.green')
+const three = document.querySelector('.blue')
+const four = document.querySelector('.yellow')
+const five = document.querySelector('.orange')
+const six = document.querySelector('.purple')
+const seven = document.querySelector('.cyan')
+const eight = document.querySelector('.indigo')
+const nine = document.querySelector('.coral')
+const ten = document.querySelector('.maroon')
+const eleven = document.querySelector('.seagreen')
+const twelve = document.querySelector('.grey')
+const thirteen = document.querySelector('.salmon')
+const fourteen = document.querySelector('.olive')
+const fifteen = document.querySelector('.navy')
+const sixteen = document.querySelector('.saddlebrown')
 
 const getRandomPanel = () => {
   const panels = [
@@ -33,52 +33,52 @@ const getRandomPanel = () => {
     fourteen,
     fifteen,
     sixteen
-  ];
-  return panels[parseInt(Math.random() * panels.length)];
-};
+  ]
+  return panels[parseInt(Math.random() * panels.length)]
+}
 
-const sequence = [getRandomPanel()];
-let sequenceToGuess = [...sequence];
+const sequence = [getRandomPanel()]
+let sequenceToGuess = [...sequence]
 
 const flash = panel => {
   return new Promise((resolve, reject) => {
-    panel.className += ' active';
-    setTimeout (() => {
+    panel.className += ' active'
+    setTimeout(() => {
       panel.className = panel.className.replace(
         ' active',
         ''
-      );
+      )
       setTimeout(() => {
-        resolve();
-      }, 200);
-    }, 500);
-  });
-};
+        resolve()
+      }, 200)
+    }, 500)
+  })
+}
 
-let canClick = false;
+let canClick = false
 
 const panelClicked = panelClicked => {
-  if (!canClick) return;
-  const expectedPanel = sequenceToGuess.shift();
+  if (!canClick) return
+  const expectedPanel = sequenceToGuess.shift()
   if (expectedPanel === panelClicked) {
     if (sequenceToGuess.length === 0) {
-      //the start of a new round
-      sequence.push(getRandomPanel());
-      sequenceToGuess = [...sequence];
-      startFlashing();
+      // the start of a new round
+      sequence.push(getRandomPanel())
+      sequenceToGuess = [...sequence]
+      startFlashing()
     }
   } else {
-    //end of game
-    alert('Game Over!');
+    // end of game
+    alert('Game Over!')
   }
-};
+}
 
 const startFlashing = async () => {
-  canClick = false;
+  canClick = false
   for (const panel of sequence) {
-    await flash(panel);
+    await flash(panel)
   }
-  canClick = true;
-};
+  canClick = true
+}
 
-startFlashing();
+startFlashing()
